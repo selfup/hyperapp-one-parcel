3a651715b392ad0904e28b0c28ff512307f7d0ed
selfup <boudinot.regis@yahoo.com>
2018-02-18 12:19:09 -0600

    Format actions test

--- test/actions.test.js
+++ test/actions.test.js
@@ -2,17 +2,17 @@ import actions from './../src/actions';
 import state from './../src/state';
 
 test('Actions up', () => {
-    const addResult = actions.up()(state);
+  const addResult = actions.up()(state);
 
-    expect(addResult).toEqual({
-        num: 1,
-    });
+  expect(addResult).toEqual({
+    num: 1,
+  });
 });
 
 test('Actions down', () => {
-    const addResult = actions.down()(state);
+  const addResult = actions.down()(state);
 
-    expect(addResult).toEqual({
-        num: -1,
-    });
+  expect(addResult).toEqual({
+    num: -1,
+  });
 });
